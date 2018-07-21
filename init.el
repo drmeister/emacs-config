504815fb5b6b5a53ec97b83afaf43d236acba9cc
drmeister <chris.schaf@verizon.net>
2018-07-20 22:50:24 -0400

    Moved stuff from init.el to dot_emacs

--- init.el
+++ init.el
@@ -100,7 +100,7 @@
 (message "Loading slime")
 (add-to-list 'load-path "~/.emacs.d/slime")
 ;(setq slime-contribs '(slime-fancy slime-scratch slime-asdf))
-(setq slime-contribs '(slime-fancy slime-scratch))
+;(setq slime-contribs '(slime-fancy slime-scratch))
 ;;;(slime-setup '(slime-scratch slime-fancy slime-asdf))
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy slime-tramp slime-indentation))
@@ -241,5 +241,36 @@
 (setq auto-mode-alist (cons '("\\wscript$" . python-mode) auto-mode-alist))
 
 
+;;; Code to convert camelcase to dash
+(defun split-name (s)
+  (split-string
+   (let ((case-fold-search nil))
+     (downcase
+      (replace-regexp-in-string "\\([a-z]\\)\\([A-Z]\\)" "\\1 \\2" s)))
+   "[^A-Za-z0-9]+"))
+
+(defun camelcase  (s) (mapconcat 'capitalize (split-name s) ""))
+(defun underscore (s) (mapconcat 'downcase   (split-name s) "_"))
+(defun dasherize  (s) (mapconcat 'downcase   (split-name s) "-"))
+
+(defun camelscore-word-at-point ()
+  (interactive)
+  (let* ((case-fold-search nil)
+         (beg (and (skip-chars-backward "[:alnum:]_-") (point)))
+         (end (and (skip-chars-forward  "[:alnum:]_-") (point)))
+         (txt (buffer-substring beg end))
+         (cml (dasherize txt)) )
+    (if cml (progn (delete-region beg end) (insert cml))) ))
+
+
+(global-set-key (kbd "C-c i") 'camelscore-word-at-point)
+
+
+
+
 (message "Done with init.el")
+
+
+
+
 
